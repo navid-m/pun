f6b92ed7b85df2d25aa9b1538e2116112d08af29
Navid M <navidmomtahen@gmail.com>
2026-02-14 10:41:31 +0000

    add static

--- source/app.d
+++ source/app.d
@@ -8,11 +8,11 @@ import std.algorithm;
 import std.array;
 import std.conv;
 
-immutable string PUN_HOME = ".pun";
-immutable string PERLS_DIR = "perls";
-immutable string PROJECT_CONFIG = ".punrc";
-immutable string PROJECT_LOCAL_CONFIG = ".punlrc";
-immutable string PROJECT_LOCK = "pun.lock";
+static immutable string PUN_HOME = ".pun";
+static immutable string PERLS_DIR = "perls";
+static immutable string PROJECT_CONFIG = ".punrc";
+static immutable string PROJECT_LOCAL_CONFIG = ".punlrc";
+static immutable string PROJECT_LOCK = "pun.lock";
 
 void main(string[] args)
 {
